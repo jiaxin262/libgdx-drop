1c830d0ef8c15356af6b7014569942bf769f34cf
jiaxin <jiaxin@jiaxindeMacBook-Pro.local>
2017-02-08 10:49:35 +0800

    add

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -8,7 +8,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 public class Drop extends ApplicationAdapter {
 
@@ -20,6 +26,9 @@ public class Drop extends ApplicationAdapter {
     private SpriteBatch batch;
 
     private Rectangle bucket;
+    Vector3 touchPos = new Vector3();
+    private Array<Rectangle> raindrops;
+    private long lastDropTime;
 
     @Override
     public void create() {
@@ -45,6 +54,9 @@ public class Drop extends ApplicationAdapter {
         bucket.y = 20;
         bucket.width = 64;
         bucket.height = 64;
+
+        raindrops = new Array<Rectangle>();
+        spawnRaindrop();
     }
 
     @Override
@@ -57,11 +69,50 @@ public class Drop extends ApplicationAdapter {
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(bucketImage, bucket.x, bucket.y);
+        for(Rectangle raindrop: raindrops) {
+            batch.draw(dropImage, raindrop.x, raindrop.y);
+        }
         batch.end();
+
+        if(Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            bucket.x = touchPos.x - 64 / 2;
+        }
+
+        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) {
+            spawnRaindrop();
+        }
+        Iterator<Rectangle> iter = raindrops.iterator();
+        while(iter.hasNext()) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if(raindrop.y + 64 < 0) {
+                iter.remove();
+            }
+            if(raindrop.overlaps(bucket)) {
+                dropSound.play();
+                iter.remove();
+            }
+        }
+    }
+
+    private void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800-64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
     }
 
     @Override
     public void dispose() {
-
+        dropImage.dispose();
+        bucketImage.dispose();
+        dropSound.dispose();
+        rainMusic.dispose();
+        batch.dispose();
     }
 }
